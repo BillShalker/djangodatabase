3ea9f783d6bff37dc5796b0da988aec891efb06d
Murenko Daniil <68713259+mmmurka@users.noreply.github.com>
2024-02-21 22:19:54 +0200

    Update models.py

--- catalog/models.py
+++ catalog/models.py
@@ -4,10 +4,10 @@ from django.db import models
 class Catalog(models.Model):
     # Определите поля модели, соответствующие полям вашей таблицы
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    value = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
+    name = models.CharField(max_length=255, blank=False, null=False)
+    value = models.IntegerField(blank=False, null=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    total = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
 
     class Meta:
         managed = False  # Указывает Django не управлять созданием таблицы
